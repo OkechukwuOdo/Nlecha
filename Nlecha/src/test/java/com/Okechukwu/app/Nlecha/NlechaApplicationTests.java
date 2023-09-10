0b4843729622772019dbd4afa7b899cc0112e013
OkechukwuOdo <okechukwuoodo@gmail.com>
2023-09-10 12:43:32 +0100

    first asaervice

---
+++ Nlecha/src/test/java/com/Okechukwu/app/Nlecha/NlechaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Okechukwu.app.Nlecha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlechaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
